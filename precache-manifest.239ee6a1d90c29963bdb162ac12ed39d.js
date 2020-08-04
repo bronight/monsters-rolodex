a1200816922fb9b3f6e02b02c9a01786fb8cf61c
Louis Williams <louawilliams@outlook.com>
2020-08-03 22:27:54 -0400

    Updates

---
+++ precache-manifest.239ee6a1d90c29963bdb162ac12ed39d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacb94d280aeb8f37aa78a2a74e51a4d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "439cacc3c4e36ea92017",
+    "url": "/monsters-rolodex/static/css/main.4ca7b46e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439cacc3c4e36ea92017",
+    "url": "/monsters-rolodex/static/js/main.d04f8dcc.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
